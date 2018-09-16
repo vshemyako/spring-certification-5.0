f7b7ae6d809c35fe973029d6aeec65b294dce433
Valentine Shemyako <v.shemyako@gmail.com>
2018-09-16 13:31:43 +0300

    Created message factory class, coupled with properties file

---
+++ src/main/java/spring/chapter01/factory/MessageFactory.java
@@ -0,0 +1,60 @@
+package spring.chapter01.factory;
+
+import spring.chapter01.decoupled.MessageProvider;
+import spring.chapter01.decoupled.MessageRenderer;
+
+import java.util.ResourceBundle;
+
+/**
+ * Further extended decoupled example which chooses concrete interface implementations
+ * from a property file
+ */
+public class MessageFactory {
+
+    private static final String RESOURCE_BUNDLE_NAME = "factory";
+    private static final String PROVIDER = "provider";
+    private static final String RENDERER = "renderer";
+
+    private static MessageFactory instance = new MessageFactory();
+
+    private MessageProvider messageProvider;
+    private MessageRenderer messageRenderer;
+
+    /**
+     * Instantiates 'message' type classes on application start-up, reading concrete implementation classes
+     * from properties file
+     */
+    private MessageFactory() {
+        try {
+            ResourceBundle resourceBundle = ResourceBundle.getBundle(RESOURCE_BUNDLE_NAME);
+            this.messageProvider = (MessageProvider) Class.forName(resourceBundle.getString(PROVIDER)).newInstance();
+            this.messageRenderer = (MessageRenderer) Class.forName(resourceBundle.getString(RENDERER)).newInstance();
+        } catch (Exception ex) {
+            System.err.println("Failed to instantiate message factory: " + ex.getMessage());
+            throw new IllegalStateException("Application is in an undefined state. Review application config.");
+        }
+    }
+
+    /**
+     * Eager singleton design pattern implementation (not actually the best choice)
+     */
+    public static MessageFactory getInstance() {
+        return instance;
+    }
+
+    /**
+     * @return {@link MessageProvider} instance, concrete implementation class
+     * is defined within properties file
+     */
+    public MessageProvider getMessageProvider() {
+        return messageProvider;
+    }
+
+    /**
+     * @return {@link MessageRenderer} instance, concrete implementation class
+     * is defined within properties file
+     */
+    public MessageRenderer getMessageRenderer() {
+        return messageRenderer;
+    }
+}
