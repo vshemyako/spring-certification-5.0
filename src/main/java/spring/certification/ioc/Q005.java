cd81380c8f1c78b97c4efa081fca1e4d5c172d2b
Valentine Shemyako <v.shemyako@gmail.com>
2018-11-24 13:13:26 +0300

    Added explanation to question006

--- src/main/java/spring/certification/ioc/Q005.java
+++ src/main/java/spring/certification/ioc/Q005.java
@@ -3,10 +3,12 @@ package spring.certification.ioc;
 import org.springframework.beans.factory.Aware;
 import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
