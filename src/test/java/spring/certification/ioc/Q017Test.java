5af0efb8e0c5a551e0f597ad52badd0ce61e3417
Valentine Shemyako <v.shemyako@gmail.com>
2018-11-24 15:43:19 +0300

    Added implementation of a bean post processor interface, added unit tests

---
+++ src/test/java/spring/certification/ioc/Q017Test.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+/**
+ * Spring v5.0 Professional Certification.
+ * <p>
+ * Question 000:<br>
+ * -<br>
+ * <p>
+ * Answer:<br>
+ * -<br>
+ * <p>
+ * Examples of mentioned terms:<br>
+ * -
+ *
+ * @author Valentine Shemyako
+ * @since Ноябрь 24, 2018
+ */
+public class Q017Test {
+
+}
