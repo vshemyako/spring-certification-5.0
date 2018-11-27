b02c23f8190c215db9e2f7be6448339fc7f004ac
Valentine Shemyako <v.shemyako@gmail.com>
2018-11-27 23:42:14 +0300

    Added examples of using Qualifier meta-annotation

--- src/main/java/spring/certification/ioc/Q012.java
+++ src/main/java/spring/certification/ioc/Q012.java
@@ -1,10 +1,14 @@
 package spring.certification.ioc;
 
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
@@ -25,10 +29,109 @@ import java.lang.annotation.Target;
  * Spring beans.<br>
  * <p>
  * Examples of mentioned terms:<br>
- * -
+ * {@link Q012.NonQualifiedConfiguration} - demonstrates wrong configuration class with DI ambiguity.<br>
+ * {@link Q012.QualifiedConfiguration} - demonstrates appropriate configuration class which properly uses custom
+ * {@link Qualifier} annotation.
  *
  * @author Valentine Shemyako
  * @since November 27, 2018
  */
 public class Q012 {
+
+    /**
+     * Configuration class which do not use {@link Qualifier} annotation properly and therefore
+     * is not appropriate as meta-data provider for application-context.
+     */
+    @Configuration
+    public static class NonQualifiedConfiguration {
+
+        @Bean
+        public Cinema cinema(Film film) {
+            return new Cinema(film);
+        }
+
+        @Bean
+        public Film comedyFilm(@Value("Pulp Fiction") String filmName) {
+            return new Film(filmName);
+        }
+
+        @Bean
+        public Film actionFilm(@Value("Fight Club") String filmName) {
+            return new Film(filmName);
+        }
+    }
+
+    /**
+     * Configuration class which uses {@link Qualifier} annotation properly.
+     */
+    @Configuration
+    public static class QualifiedConfiguration {
+
+        @Bean
+        public Cinema cinema(@Genre(FilmGenre.ACTION) Film film) {
+            return new Cinema(film);
+        }
+
+        @Genre(FilmGenre.COMEDY)
+        @Bean
+        public Film comedyFilm(@Value("Pulp Fiction") String filmName) {
+            return new Film(filmName);
+        }
+
+        @Genre(FilmGenre.ACTION)
+        @Bean
+        public Film actionFilm(@Value("Fight Club") String filmName) {
+            return new Film(filmName);
+        }
+    }
+
+    /**
+     * Custom annotation type which can be used for more granular dependency injection.
+     * {@link Qualifier} annotation is used as meta-annotation here.
+     */
+    @Target({ElementType.TYPE, ElementType.METHOD, ElementType.PARAMETER})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Qualifier
+    public @interface Genre {
+        FilmGenre value();
+    }
+
+    /**
+     * Defines the most common list of film genres.
+     */
+    public enum FilmGenre {
+        ACTION, COMEDY, DRAMA
+    }
+
+    /**
+     * A motion picture.
+     */
+    public static class Film {
+
+        private String filmName;
+
+        public Film(String filmName) {
+            this.filmName = filmName;
+        }
+
+        public String getFilmName() {
+            return filmName;
+        }
+    }
+
+    /**
+     * A movie theater.
+     */
+    public static class Cinema {
+
+        private Film film;
+
+        public Cinema(Film film) {
+            this.film = film;
+        }
+
+        public Film getFilm() {
+            return film;
+        }
+    }
 }
