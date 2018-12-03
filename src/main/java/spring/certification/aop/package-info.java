6dd7eaccaa97f3e85669b8fd1fb5b4214e96aa03
Valentine Shemyako <v.shemyako@gmail.com>
2018-12-03 22:10:48 +0300

    Added package info of AOP chapter

---
+++ src/main/java/spring/certification/aop/package-info.java
@@ -0,0 +1,29 @@
+/**
+ * Spring v5.0 Professional Certification.
+ * <p>
+ * Table of contents:<br>
+ * 1.1. What is the concept of AOP?<br>
+ * 1.2. What problems does AOP solve?<br>
+ * 1.3. What is cross cutting concern?<br>
+ * 1.4. Name three typical cross cutting concerns.<br>
+ * 1.5. What two problems arise if you don't solve a cross cutting concern via AOP?<br>
+ * 2. What is a pointcut, a join point, an advice, an aspect, a weaving?<br>
+ * 3. How does Spring framework solve a cross cutting concern?<br>
+ * 4.1. What are the limitations of the two proxy types?<br>
+ * 4.2. What visibility must Spring bean methods have to be proxied using Spring AOP?<br>
+ * 5.1. How many advice types does Spring support. Can you name each one?<br>
+ * 5.2. What different advice types are used for?<br>
+ * 5.3. Which types of advices can you use if you would like to try and catch exceptions?<br>
+ * 6.1. What do you have to do to enable the detection of the @Aspect annotation?<br>
+ * 6.2. What does @EnableAspectJAutoProxy do?<br>
+ * 7.1. If shown pointcut expressions, would you understand them?<br>
+ * 7.2. For example, in the course we matched getter methods on Spring Beans, what would be the correct pointcut<br>
+ * expression to match both getter and setter methods?<br>
+ * 8. What is the JoinPoint argument used for?<br>
+ * 9.1. What is a ProceedingJoinPoint?<br>
+ * 9.2. When ProceedingJoinPoint is used?<br>
+ *
+ * @author Valentine Shemyako
+ * @since December 03, 2018
+ */
+package spring.certification.aop;
