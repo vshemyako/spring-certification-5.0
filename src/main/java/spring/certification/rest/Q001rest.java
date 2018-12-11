9d4ebb22c706cf0218d588ca6bcb8a4a5ce48383
Valentine Shemyako <v.shemyako@gmail.com>
2018-12-11 23:47:30 +0300

    Added basic explanation of what REST is

---
+++ src/main/java/spring/certification/rest/Q001rest.java
@@ -0,0 +1,30 @@
+package spring.certification.rest;
+
+/**
+ * Spring v5.0 Professional Certification.
+ * <p>
+ * Question 001:<br>
+ * What does REST stand for?<br>
+ * <p>
+ * Answer:<br>
+ * <b>REST</b> stands for Representational State Transfer. It's an architectural style for designing distributed systems.
+ * Simply put Representational State Transfer is about <b>transferring</b> of <b>representations</b> of a resource state
+ * (e.g. a rdbms record in particular <b>state</b> is serialized into JSON <b>representation</b> which is <b>transferred</b>
+ * to a client).<br>
+ * <p>
+ * Core principles of REST are:<br>
+ * 1. Client-server<br>
+ * 2. Stateless<br>
+ * 3. Cacheable<br>
+ * 4. Uniform interface<br>
+ * 5. Layered system<br>
+ * 6. Code on demand<br>
+ * <p>
+ * Examples of mentioned terms:<br>
+ * -
+ *
+ * @author Valentine Shemyako
+ * @since December 11, 2018
+ */
+public class Q001rest {
+}
