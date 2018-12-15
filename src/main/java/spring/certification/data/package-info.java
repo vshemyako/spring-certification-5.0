cf5670e705881e6ac65f60b835b92326688fdeab
Valentine Shemyako <v.shemyako@gmail.com>
2018-12-15 16:52:15 +0300

    Added explanation of checked and unchecked exceptions

---
+++ src/main/java/spring/certification/data/package-info.java
@@ -0,0 +1,14 @@
+/**
+ * Spring v5.0 Professional Certification.
+ * <p>
+ * Table of contents:<br>
+ * 1.1. What's the difference between checked and unchecked exceptions?<br>
+ * 1.2. Why does Spring prefer unchecked exceptions?<br>
+ * 1.3. What is the data access exception hierarchy?<br>
+ * 2.1. How do you configure a DataSource in Spring?<br>
+ * 2.2.  Which bean is very useful for development/test databases?<br>
+ *
+ * @author Valentine Shemyako
+ * @since December 15, 2018
+ */
+package spring.certification.data;
