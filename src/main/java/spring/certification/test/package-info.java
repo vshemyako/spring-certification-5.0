71ea59bba0ac553924a7e070f98187dce5b419da
Valentine Shemyako <v.shemyako@gmail.com>
2018-12-30 14:48:44 +0300

    Added list of questions for spring-test module

---
+++ src/main/java/spring/certification/test/package-info.java
@@ -0,0 +1,18 @@
+/**
+ * Spring v5.0 Professional Certification.
+ * <p>
+ * Table of contents:<br>
+ * 1. Do you use Spring in a unit test?<br>
+ * 2. What type of tests typically use Spring?<br>
+ * 3. How can you create a shared application context in a JUnit integration test?<br>
+ * 4. When and where do you use @Transactional in testing?<br>
+ * 5. How are mock frameworks such as Mockito or EasyMock used?<br>
+ * 6. How is @ContextConfiguration used?<br>
+ * 7. How does Spring Boot simplify writing tests?<br>
+ * 8.1. What does @SpringBootTest do?<br>
+ * 8.2. How does it interact with @SpringBootApplication and @SpringBootConfiguration?<br>
+ *
+ * @author Valentine Shemyako
+ * @since December 30, 2018
+ */
+package spring.certification.test;
