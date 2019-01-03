b48e579200b635b7a13087621405c7b286be1ba1
Valentine Shemyako <v.shemyako@gmail.com>
2019-01-03 14:20:45 +0300

    Added explanation of @ContextConfiguration usage

--- src/main/java/spring/certification/test/Q006configuration.java
+++ src/main/java/spring/certification/test/Q006configuration.java
@@ -1,5 +1,9 @@
 package spring.certification.test;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
 /**
  * Spring v5.0 Professional Certification.
  * <p>
@@ -7,7 +11,12 @@ package spring.certification.test;
  * How is @ContextConfiguration used?<br>
  * <p>
  * Answer:<br>
- * -<br>
+ * {@link ContextConfiguration} annotation specifies xml or java based configuration which should be loaded by
+ * {@link ApplicationContext} class. It is used along with {@link SpringRunner} class to test particular context
+ * configuration (presumably not so complex as production-one, only some kind of a subset).<br>
+ * <p>
+ * There're integration tests in the related project section which investigates usages of {@link ContextConfiguration}
+ * annotation.<br>
  *
  * @author Valentine Shemyako
  * @since January 02, 2019
