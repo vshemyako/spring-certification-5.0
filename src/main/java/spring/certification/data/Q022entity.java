1458f4847c25aa40613243db2b603db5d52cf6bb
Valentine Shemyako <v.shemyako@gmail.com>
2019-01-07 21:03:06 +0300

    Explained what is an entity-class

--- src/main/java/spring/certification/data/Q022entity.java
+++ src/main/java/spring/certification/data/Q022entity.java
@@ -1,5 +1,8 @@
 package spring.certification.data;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  * Spring v5.0 Professional Certification.
  * <p>
@@ -14,9 +17,26 @@ package spring.certification.data;
  * An <b>EntityManager</b> is essentially an API to access <b>PersistenceContext</b>. Each <b>EntityManager</b> is
  * associated with a <b>PersistenceContext</b>.<br>
  * <p>
+ * {@link Entity} annotation can be applied at a <b>class-level</b> only. Only annotated class is considered to be
+ * an <b>entity-class</b> (according to JPA specification) and is used to be mapped to a database record.<br>
  *
  * @author Valentine Shemyako
  * @since January 07, 2019
  */
 public class Q022entity {
+
+    /**
+     * An example of an entity-class. The following is required for a class to be an entity-class:<br>
+     * - a class must be annotated with {@link Entity}<br>
+     * - a class must have an {@link Id} field<br>
+     * - a class must have a public or protected, no-argument constructor<br>
+     * - a class must not be declared final<br>
+     */
+    @Entity
+    public static class Human {
+        @Id
+        private long id;
+        private String firstName;
+        private String lastName;
+    }
 }
