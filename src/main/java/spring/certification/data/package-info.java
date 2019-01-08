c6d082a433196d8245ba19917a419ebdac313b39
Valentine Shemyako <v.shemyako@gmail.com>
2019-01-08 23:33:06 +0300

    Explained possible solutions for JPA transactions support

--- src/main/java/spring/certification/data/package-info.java
+++ src/main/java/spring/certification/data/package-info.java
@@ -48,7 +48,7 @@
  * 22.2. Why do you need the @Entity annotation. Where can it be placed? {@link spring.certification.data.Q022entitymanager link}<br>
  * 23. What do you need to do in Spring if you would like to work with JPA? {@link spring.certification.data.Q023entitymanagerfactory link}<br>
  * 24. Are you able to participate in a given transaction in Spring while working with JPA?<br>
- * 25. Which PlatformTransactionManager(s) can you use with JPA?<br>
+ * 25. Which PlatformTransactionManager(s) can you use with JPA? {@link spring.certification.data.Q025jpatransaction link}<br>
  * 26. What does @PersistenceContext do? {@link spring.certification.data.Q026persistencecontext link}<br>
  * 27.1. What do you have to configure to use JPA with Spring? {@link spring.certification.data.Q027springbootjpaconfig link}<br>
  * 27.2. How does Spring Boot make this easier? {@link spring.certification.data.Q027springbootjpaconfig link}<br>
