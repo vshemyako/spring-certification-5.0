adba7fdbef562b56db87809f795d6ebe7a067a9f
Valentine Shemyako <v.shemyako@gmail.com>
2019-01-10 21:38:43 +0300

    Created mvc package info file

---
+++ src/main/java/spring/certification/mvc/package-info.java
@@ -0,0 +1,32 @@
+/**
+ * Spring v5.0 Professional Certification.
+ * <p>
+ * Table of contents:<br>
+ * 1. MVC is an abbreviation for a design pattern. What does it stand for and what is the idea behind it?<br>
+ * 2. Do you need spring-mvc.jar in your classpath or is it part of spring-core?<br>
+ * 3. What is the DispatcherServlet and what is it used for?<br>
+ * 4. Is the DispatcherServlet instantiated via an application context?<br>
+ * 5. What is a web application context? What extra scopes does it offer?<br>
+ * <p>
+ * 6. What is the @Controller annotation used for?<br>
+ * 7. How is an incoming request mapped to a controller and mapped to a method?<br>
+ * 8. What is the difference between @RequestMapping and @GetMapping?<br>
+ * 9. What is @RequestParam used for?<br>
+ * 10. What are the differences between @RequestParam and @PathVariable?<br>
+ * 11. What are some of the parameter types for a controller method?<br>
+ * 12. What other annotations might you use on a controller method parameter?<br>
+ * 13. What are some of the valid return types of a controller method?<br>
+ * <p>
+ * 14. What is a View and what's the idea behind supporting different types of View?<br>
+ * 15. How is the right View chosen when it comes to the rendering phase?<br>
+ * 16. What is the Model?<br>
+ * 17. Why do you have access to the model in your View? Where does it come from?<br>
+ * 18. What is the purpose of the session scope?<br>
+ * 19. What is the default scope in the web context?<br>
+ * 20. Why are controllers testable artifacts?<br>
+ * 21. What does a ViewResolver do?<br>
+ *
+ * @author Valentine Shemyako
+ * @since January 10, 2019
+ */
+package spring.certification.mvc;
