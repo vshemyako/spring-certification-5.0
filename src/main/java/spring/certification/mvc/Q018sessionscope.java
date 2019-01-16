dd591506baa81e90c56fba0bf87807f689fefac0
Valentine Shemyako <v.shemyako@gmail.com>
2019-01-16 21:51:33 +0300

    Explained session-scoped bean lifecycle

---
+++ src/main/java/spring/certification/mvc/Q018sessionscope.java
@@ -0,0 +1,21 @@
+package spring.certification.mvc;
+
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.context.annotation.SessionScope;
+
+/**
+ * Spring v5.0 Professional Certification.
+ * <p>
+ * Question 018:<br>
+ * What is the purpose of the session scope?<br>
+ * <p>
+ * Answer:<br>
+ * Additional bean scopes which are available within {@link WebApplicationContext} are mention
+ * in {@link Q005webapplicationcontext}. {@link SessionScope} is a bean scope which is unique
+ * within single http-session.<br>
+ *
+ * @author Valentine Shemyako
+ * @since January 16, 2019
+ */
+public class Q018sessionscope {
+}
--- src/main/java/spring/certification/mvc/package-info.java
+++ src/main/java/spring/certification/mvc/package-info.java
@@ -21,10 +21,10 @@
  * 15. How is the right View chosen when it comes to the rendering phase? {@link spring.certification.mvc.Q015viewresolution link}<br>
  * 16. What is the Model? {@link spring.certification.mvc.Q016model link}<br>
  * 17. Why do you have access to the model in your View? Where does it come from? {@link spring.certification.mvc.Q017viewmodelrelation link}<br>
- * 18. What is the purpose of the session scope?<br>
+ * 18. What is the purpose of the session scope? {@link spring.certification.mvc.Q018sessionscope link}<br>
  * 19. What is the default scope in the web context? {@link spring.certification.mvc.Q019defaultscope link}<br>
  * 20. Why are controllers testable artifacts? {@link spring.certification.mvc.Q020controllertest link}<br>
- * 21. What does a ViewResolver do?<br>
+ * 21. What does a ViewResolver do? {@link spring.certification.mvc.Q021viewresolver link}<br>
  *
  * @author Valentine Shemyako
  * @since January 10, 2019
