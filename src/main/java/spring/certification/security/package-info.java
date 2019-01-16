dc979a5fa5970f53e5487ca67daf4db99ff31ad2
Valentine Shemyako <v.shemyako@gmail.com>
2019-01-16 22:12:16 +0300

    Created package-info file, which contains security chapter certification questions

---
+++ src/main/java/spring/certification/security/package-info.java
@@ -0,0 +1,32 @@
+/**
+ * Spring v5.0 Professional Certification.
+ * <p>
+ * Table of contents:<br>
+ * 1. What are authentication and authorization? Which must come first?<br>
+ * 2.1. Is security a cross cutting concern?<br>
+ * 2.2. How is it implemented internally?<br>
+ * 3. What is the delegating filter proxy?<br>
+ * 4. What is the security filter chain?<br>
+ * 5. What is a security context?<br>
+ * <p>
+ * 6. Why do you need the intercept-url?<br>
+ * 7. In which order do you have to write multiple intercept-url's?<br>
+ * 8. What does the ** pattern in an antMatcher or mvcMatcher do?<br>
+ * 9. Why is an mvcMatcher more secure than an antMatcher?<br>
+ * 10.1. Does Spring Security support password hashing?<br>
+ * 10.2. What is salting?<br>
+ * <p>
+ * 11.1. Why do you need method security?<br>
+ * 11.2. What type of object is typically secured at the method level (think of its purpose not its Java type).<br>
+ * 12. What do @PreAuthorized and @RolesAllowed do? What is the difference between them?<br>
+ * 13. What does Spring’s @Secured do?<br>
+ * 14. How are these annotations implemented?<br>
+ * 15. In which security annotation are you allowed to use SpEL?<br>
+ * 16. Is it enough to hide sections of my output (e.g. JSP-Page or Mustache template)?<br>
+ * 17. Spring security offers a security tag library for JSP, would you recognize it if you saw it
+ * in an example?<br>
+ *
+ * @author Valentine Shemyako
+ * @since January 16, 2019
+ */
+package spring.certification.security;
