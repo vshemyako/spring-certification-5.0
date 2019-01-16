2453aa464a26e4add46acd00be4b9f246a4c7e08
Valentine Shemyako <v.shemyako@gmail.com>
2019-01-16 12:09:10 +0300

    Explained usage of RequestParam annotation for annotated handlers

---
+++ src/main/java/spring/certification/mvc/Q009requestparam.java
@@ -0,0 +1,23 @@
+package spring.certification.mvc;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Spring v5.0 Professional Certification.
+ * <p>
+ * Question 009:<br>
+ * What is @RequestParam used for?<br>
+ * <p>
+ * Answer:<br>
+ * {@link RequestParam} annotation is used to bind a request parameter to a method parameter.
+ * Binding of the following types of request parameters are supported:<br>
+ * - query parameters (?position=developer)<br>
+ * - form data<br>
+ * - parts of multipart request<br>
+ * By default request parameter is required, leading to an exception being thrown if one is missing.<br>
+ *
+ * @author Valentine Shemyako
+ * @since January 16, 2019
+ */
+public class Q009requestparam {
+}
