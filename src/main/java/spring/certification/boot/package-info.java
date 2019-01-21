ed378ee628e4d7694ef1826852812bd4f11f1a17
Valentine Shemyako <v.shemyako@gmail.com>
2019-01-21 11:37:24 +0300

    Listed questions of Spring Boot certification chapter

---
+++ src/main/java/spring/certification/boot/package-info.java
@@ -0,0 +1,27 @@
+/**
+ * Spring v5.0 Professional Certification.
+ * <p>
+ * Table of contents:<br>
+ * 1. What is Spring Boot?<br>
+ * 2. What are the advantages of using Spring Boot?<br>
+ * 3. Why is Spring Boot “opinionated”?<br>
+ * 4. How does Spring Boot work? How does it know what to configure?<br>
+ * 5. What things affect what Spring Boot sets up?<br>
+ * 6.1. How are properties defined?<br>
+ * 6.2. Where is Spring Boot’s default property source?<br>
+ * 7. Would you recognize common Spring Boot annotations and configuration properties if you saw them in the exam?<br>
+ * 8. What is the difference between an embedded container and a WAR?<br>
+ * 9. What embedded containers does Spring Boot support?<br>
+ * 10. What does @EnableAutoConfiguration do?<br>
+ * 11. What does @SpringBootApplication do?<br>
+ * 12.1. Does Spring Boot do component scanning?<br>
+ * 12.2. Where does it look by default?<br>
+ * 13.1. What is a Spring Boot starter POM?<br>
+ * 13.2. Why is it useful?<br>
+ * 14. Spring Boot supports both Java properties and YML files. Would you recognize and understand them if you saw them?<br>
+ * 15. How can you control logging with Spring Boot?<br>
+ *
+ * @author Valentine Shemyako
+ * @since January 21, 2018
+ */
+package spring.certification.boot;
