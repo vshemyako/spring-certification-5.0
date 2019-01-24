cf517acb1d4d27d3eb51f7ddb7c6aed648203601
Valentine Shemyako <v.shemyako@gmail.com>
2019-01-24 12:41:53 +0300

    Formatted package-info file

--- src/main/java/spring/certification/aop/package-info.java
+++ src/main/java/spring/certification/aop/package-info.java
@@ -2,29 +2,22 @@
  * Spring v5.0 Professional Certification.
  * <p>
  * Table of contents:<br>
- * 1.1. What is the concept of AOP? {@link spring.certification.aop.Q001concepts link}<br>
- * 1.2. What problems does AOP solve? {@link spring.certification.aop.Q001concepts link}<br>
- * 1.3. What is cross cutting concern? {@link spring.certification.aop.Q001concepts link}<br>
- * 1.4. Name three typical cross cutting concerns. {@link spring.certification.aop.Q001concepts link}<br>
- * 1.5. What two problems arise if you don't solve a cross cutting concern via AOP?
- * {@link spring.certification.aop.Q001concepts link}<br>
- * 2. What is a pointcut, a join point, an advice, an aspect, a weaving?
- * {@link spring.certification.aop.Q002terminology link}<br>
+ * 1.1. What is the concept of AOP? {@link spring.certification.aop.q001.AOPConcepts link}<br>
+ * 1.2. What problems does AOP solve? {@link spring.certification.aop.q001.AOPConcepts link}<br>
+ * 1.3. What is cross cutting concern? {@link spring.certification.aop.q001.AOPConcepts link}<br>
+ * 1.4. Name three typical cross cutting concerns. {@link spring.certification.aop.q001.AOPConcepts link}<br>
+ * 1.5. What two problems arise if you don't solve a cross cutting concern via AOP? {@link spring.certification.aop.q001.AOPConcepts link}<br>
+ * 2. What is a pointcut, a join point, an advice, an aspect, a weaving? {@link spring.certification.aop.Q002terminology link}<br>
  * 3. How does Spring framework solve a cross cutting concern? {@link spring.certification.aop.Q003proxy link}<br>
  * 4.1. What are the limitations of the two proxy types? {@link spring.certification.aop.Q004limitations link}<br>
- * 4.2. What visibility must Spring bean methods have to be proxied using Spring AOP?
- * {@link spring.certification.aop.Q004limitations link}<br>
- * 5.1. How many advice types does Spring support. Can you name each one?
- * {@link spring.certification.aop.Q005advice link}<br>
+ * 4.2. What visibility must Spring bean methods have to be proxied using Spring AOP? {@link spring.certification.aop.Q004limitations link}<br>
+ * 5.1. How many advice types does Spring support. Can you name each one? {@link spring.certification.aop.Q005advice link}<br>
  * 5.2. What different advice types are used for? {@link spring.certification.aop.Q005advice link}<br>
- * 5.3. Which types of advices can you use if you would like to try and catch exceptions?
- * {@link spring.certification.aop.Q005advice link}<br>
- * 6.1. What do you have to do to enable the detection of the @Aspect annotation?
- * {@link spring.certification.aop.Q006aspectj link}<br>
+ * 5.3. Which types of advices can you use if you would like to try and catch exceptions? {@link spring.certification.aop.Q005advice link}<br>
+ * 6.1. What do you have to do to enable the detection of the @Aspect annotation? {@link spring.certification.aop.Q006aspectj link}<br>
  * 6.2. What does @EnableAspectJAutoProxy do? {@link spring.certification.aop.Q006aspectj link}<br>
  * 7.1. If shown pointcut expressions, would you understand them? {@link spring.certification.aop.Q007pointcut link}<br>
- * 7.2. For example, in the course we matched getter methods on Spring Beans, what would be the correct pointcut
- * expression to match both getter and setter methods? {@link spring.certification.aop.Q007pointcut link}<br>
+ * 7.2. For example, in the course we matched getter methods on Spring Beans, what would be the correct pointcut expression to match both getter and setter methods? {@link spring.certification.aop.Q007pointcut link}<br>
  * 8. What is the JoinPoint argument used for? {@link spring.certification.aop.Q008joinpoint link}<br>
  * 9.1. What is a ProceedingJoinPoint? {@link spring.certification.aop.Q009proceedingjoinpoint link}<br>
  * 9.2. When ProceedingJoinPoint is used? {@link spring.certification.aop.Q009proceedingjoinpoint link}<br>
