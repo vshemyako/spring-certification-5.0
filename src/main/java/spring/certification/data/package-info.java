7f014c381f9ae2f4ff4774f74ad890081a45224e
Valentine Shemyako <v.shemyako@gmail.com>
2019-01-26 12:06:11 +0300

    Code cleanup

--- src/main/java/spring/certification/data/package-info.java
+++ src/main/java/spring/certification/data/package-info.java
@@ -12,8 +12,7 @@
  * 4.2. What are the three JdbcTemplate callback interfaces that can be used with queries? {@link spring.certification.data.q004.Callback link}<br>
  * 4.3. What each JdbcTemplate is used for? {@link spring.certification.data.q004.Callback link}<br>
  * 5. Can you execute a plain SQL statement with the JDBC template? {@link spring.certification.data.q005.PlainSql link}<br>
- * 6. When does the JDBC template acquire (and release) a connection  - for every method called or once per template?
- * {@link spring.certification.data.q006.ConnectionAcquireStrategy link}<br>
+ * 6. When does the JDBC template acquire (and release) a connection  - for every method called or once per template? {@link spring.certification.data.q006.ConnectionAcquireStrategy link}<br>
  * 7.1. How does the JdbcTemplate support generic queries? {@link spring.certification.data.Q007generics link}<br>
  * 7.2. How does it return objects and lists/maps of objects? {@link spring.certification.data.Q007generics link}<br>
  * <p>
