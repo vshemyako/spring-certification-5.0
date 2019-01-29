069bcd65c9bff25ccdb5c23a955479c0d242ed5a
Valentine Shemyako <v.shemyako@gmail.com>
2019-01-29 10:15:58 +0300

    Additional note about constant creation of CGLIB proxies

--- src/main/java/spring/certification/aop/q003/CrossCuttingSolution.java
+++ src/main/java/spring/certification/aop/q003/CrossCuttingSolution.java
@@ -1,5 +1,7 @@
 package spring.certification.aop.q003;
 
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
 /**
  * Spring v5.0 Professional Certification.
  * <p>
@@ -15,6 +17,9 @@ package spring.certification.aop.q003;
  * 2. CGLIB proxies - in case target object does not implement any interface, or in case all concrete object methods
  * invocations should be intercepted.<br>
  * <p>
+ * Additional info:<br>
+ * Default Spring AOP proxy type is JDK dynamic proxy. In order to make Spring AOP framework create only subclass CGLIB proxies -
+ * {@link EnableAspectJAutoProxy#proxyTargetClass()} attribute has to be set to {@code true}.<br>
  * Note: Test module contains verification logic of proxy types being created.<br>
  *
  * @author Valentine Shemyako
