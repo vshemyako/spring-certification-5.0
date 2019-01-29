1fd4499e0406a6547bd66fe17210c09add48e53d
Valentine Shemyako <v.shemyako@gmail.com>
2019-01-29 16:29:07 +0300

    Specified details of SecurityFilterChain structure

--- src/main/java/spring/certification/security/Q004securityfilterchain.java
+++ src/main/java/spring/certification/security/Q004securityfilterchain.java
@@ -3,6 +3,7 @@ package spring.certification.security;
 import javax.servlet.Filter;
 import org.springframework.security.web.FilterChainProxy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatcher;
 
 /**
  * Spring v5.0 Professional Certification.
@@ -12,9 +13,10 @@ import org.springframework.security.web.SecurityFilterChain;
  * <p>
  * Answer:<br>
  * {@link FilterChainProxy} maintains a list of {@link SecurityFilterChain} objects each of which
- * is capable to decide whether is should be applied to a web-request or not. The first filter-chain
+ * is capable to decide whether it should be applied to a web-request or not. The first filter-chain
  * which matches the request is responsible for its processing. Internally {@link SecurityFilterChain}
- * is itself a list of {@link Filter} objects.<br>
+ * is itself a list of {@link Filter} objects plus single instance of {@link RequestMatcher} which
+ * actually performs url-matching.<br>
  *
  * @author Valentine Shemyako
  * @since January 17, 2019
