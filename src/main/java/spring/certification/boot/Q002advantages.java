79eac015de3a4d895c63858ca96126fe9ef706ed
Valentine Shemyako <v.shemyako@gmail.com>
2019-01-30 15:46:02 +0300

    Added Spring Boot advantages listed in the official documentation

--- src/main/java/spring/certification/boot/Q002advantages.java
+++ src/main/java/spring/certification/boot/Q002advantages.java
@@ -12,6 +12,13 @@ package spring.certification.boot;
  * - beans autoconfiguration<br>
  * - possibility to create single jar for the entire web-application<br>
  * - easy customization of Spring Boot defaults<br>
+ * Extra:<br>
+ * Below is an excerpt taken from documentation about Spring Boot advantages:<br>
+ * - Provide a radically faster and widely accessible getting-started experience for all Spring development.<br>
+ * - Be opinionated out of the box but get out of the way quickly as requirements start to diverge from the defaults.<br>
+ * - Provide a range of non-functional features that are common to large classes of projects (such as embedded servers,
+ * security, metrics, health checks, and externalized configuration).<br>
+ * - Absolutely no code generation and no requirement for XML configuration.<br>
  *
  * @author Valentine Shemyako
  * @since January 21, 2019
